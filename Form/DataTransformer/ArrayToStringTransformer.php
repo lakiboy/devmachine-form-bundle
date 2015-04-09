167e1450da104cfc3c3d29785aec342eed07c665
Dmitri Lakachauskis <lakiboy83@gmail.com>
2015-04-09 17:27:25 +0300

    Move 'devmachine_flat_choice' form type from AdminBundle into FormBundle.

---
+++ Form/DataTransformer/ArrayToStringTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Devmachine\FormBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class ArrayToStringTransformer implements DataTransformerInterface
+{
+    private $separator;
+
+    public function __construct($separator = ', ')
+    {
+        $this->separator = $separator;
+    }
+
+    public function transform($value)
+    {
+        if ($value === null) {
+            return '';
+        }
+
+        if (!is_array($value)) {
+            throw new TransformationFailedException(sprintf('Expected array, got %s', is_scalar($value) ? gettype($value) : get_class($value)));
+        }
+
+        return implode($this->separator, $value);
+    }
+
+    public function reverseTransform($value)
+    {
+        if ($value === '' || $value === null) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new TransformationFailedException(sprintf('Expected string, got %s', is_scalar($value) ? gettype($value) : get_class($value)));
+        }
+
+        return array_map('trim', explode($this->separator, $value));
+    }
+}
---
+++ Form/Type/FlatChoiceType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Devmachine\FormBundle\Form\Type;
+
+use Devmachine\FormBundle\Form\DataTransformer\ArrayToStringTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FlatChoiceType extends AbstractType
+{
+    public function getName()
+    {
+        return 'devmachine_flat_choice';
+    }
+
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer(new ArrayToStringTransformer($options['separator']));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'multiple' => true,
+            'separator' => ', ',
+        ]);
+    }
+}
