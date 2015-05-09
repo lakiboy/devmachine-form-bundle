282fe4e40a07619e940e3f38f9afd7cc1fb54298
Dmitri Lakachauskis <lakiboy83@gmail.com>
2015-05-09 16:05:57 +0300

    New child choice form type.

---
+++ Form/Type/ChildChoiceType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Devmachine\FormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ChildChoiceType extends AbstractType
+{
+    public function getName()
+    {
+        return 'devmachine_child_choice';
+    }
+
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['group_by', 'parent']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['parent'] = $options['parent'];
+    }
+
+    /**
+     * BC.
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        /** @var \Symfony\Component\OptionsResolver\OptionsResolver $resolver */
+        $this->configureOptions($resolver);
+    }
+}
