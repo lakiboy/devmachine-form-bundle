981628bf21abebf3fa6e3607a68b44115e48f764
Dmitri Lakachauskis <lakiboy83@gmail.com>
2015-10-21 16:41:23 +0300

    FormatConfiguration test.

---
+++ Tests/FormatConfigurationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Devmachine\FormBundle\Tests;
+
+use Devmachine\FormBundle\FormatConfiguration;
+
+class FormatConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_holds_config()
+    {
+        $config = new FormatConfiguration();
+        $config->setDateFormat('Y-m-d');
+        $config->setDateTimeFormat('Y-m-d H:i:s');
+
+        $this->assertEquals('Y-m-d', $config->getDateFormat());
+        $this->assertEquals('Y-m-d H:i:s', $config->getDateTimeFormat());
+    }
+}
